f720fea87835138fcb7798900d731d174a7983ab
Washington Soares <washingtonudi@gmail.com>
2015-11-05 14:54:43 -0200

    botão de iniciar a proxima mídia

--- src/lightbox.js
+++ src/lightbox.js
@@ -4,14 +4,16 @@ angular.module('angularMasterLightbox',[])
 .directive('angularMasterLightbox', function () {
     return {
         restrict: 'E',
-        template: '<a href="{{images}}" data-lightbox="image-1" data-title="My caption"> ' +
-                      '<img style="width: 100%;max-height: 360px" src="{{images}}" alt="alternative text">'+
+        transclude: true,
+        template: '<a href="{{images}}" data-lightbox="image-1" data-title="" class="link-img-lightbox"> ' +
+                      '<img height="360" style="width: 100%;max-height: 360px" src="{{images}}" alt="">' +
                   '</a>',
         scope: {
             images: '='
         },
-        controller: function () {
-
+        link: function(scope,element,attrs,ctrl,transclude){
+            angular.element('.multi-video').prepend(transclude());
         }
+
     };
 });
